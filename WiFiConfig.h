a3683f1d3d1b856c9e16ecaa06b077d523262c41
yossi-whist <112317370+yossi-whist@users.noreply.github.com>
2023-02-23 13:23:14 +0200

    Update WiFiConfig.h

--- WiFiConfig.h
+++ WiFiConfig.h
@@ -3,7 +3,7 @@
 
 #define WIFI_SSID "Shaulzon 2.4"
 #define WIFI_PASS "0522333388"
-#define HOST_IP "10.100.102.34"
+#define HOST_IP "http://ec2-52-57-141-18.eu-central-1.compute.amazonaws.com/"
 #define HOST_PORT 80
 
 #endif
